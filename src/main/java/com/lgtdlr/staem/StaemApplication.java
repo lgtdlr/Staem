9d3020447536d49d697ef8b754c4a8a5b98a5b21
Leonardo Torres <leonardo.torres5@upr.edu>
2022-04-21 23:13:10 -0400

    Initial commit

---
+++ src/main/java/com/lgtdlr/staem/StaemApplication.java
@@ -0,0 +1,13 @@
+package com.lgtdlr.staem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaemApplication.class, args);
+    }
+
+}
